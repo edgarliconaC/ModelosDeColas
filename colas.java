c4bcb07631b761b87039a69ffd920300c85c0056
Edgar Licona Cazares <edgarliconacaz@gmail.com>
2025-12-03 00:37:09 +0000

    Colas actualizado

--- colas.java
+++ colas.java
@@ -7,7 +7,16 @@ public class Colas {
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in); 
 
-        
+        byte opcion = 0;
+        do {    
+
+            System.out.println("Menu");
+            System.out.println("Que modelo de cola desea utilizar?");
+            System.out.println("1. M/M/1");
+            System.out.println("2. ");
+
+            
+        } while (opcion != 5);
     }
 
 }

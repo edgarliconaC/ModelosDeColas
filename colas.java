b3df7296860fad043a573fd66ea2c8e3d23e474e
Edgar Licona Cazares <edgarliconacaz@gmail.com>
2025-12-03 00:22:54 +0000

    Colas actualizado

--- colas.java
+++ colas.java
@@ -1,8 +1,12 @@
 package colas;
 
+import java.util.Scanner;
+
 public class Colas {
 
     public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in); 
+
         
     }
 

fc744d4bab5a377e8498f6d2b9775be2d7929ba4
Edgar Licona Cazares <edgarliconacaz@gmail.com>
2025-12-03 01:38:33 +0000

    Colas Actualizadas

--- colas.java
+++ colas.java
@@ -7,13 +7,34 @@ public class Colas {
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in); 
 
+        double lq, p, wq, w;
+
         byte opcion = 0;
         do {    
 
             System.out.println("Menu");
             System.out.println("Que modelo de cola desea utilizar?");
             System.out.println("1. M/M/1");
-            System.out.println("2. ");
+            opcion = lector.nextByte();
+
+            switch(opcion){
+                case 1:
+                    //MM!
+                    System.out.println("Cuantos pedidos atiende por hora?");
+                    int miu = lector.nextDouble();//Pedidos atendidos por hora
+                    System.out.println("Cuantos cliente atiende por hora?");
+                    int landa = lector.nextDouble();//Clientes atendidos por hora
+
+                    //Utilizacion de sistema
+                    p = landa /miu;
+                    System.out.println("P ="+ p);
+
+
+
+
+
+                    break;
+            }
 
             
         } while (opcion != 5);
